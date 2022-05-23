4fb2eb819879129e3095d1b0aabea739d6101b80
James Chen <83474000+jameschen1029384756@users.noreply.github.com>
2022-05-22 18:38:49 -0700

    Update index.tsx

--- src/components/AccountDetails/index.tsx
+++ src/components/AccountDetails/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext } from 'react';
+import React, { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import styled, { ThemeContext } from 'styled-components';
 import { useActiveWeb3React } from '../../hooks';
